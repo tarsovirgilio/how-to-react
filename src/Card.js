a6c3f319024279cafeaf73503c9eb68c158e666c
Tarso Silva <tarsovirgilioc@gmail.com>
2022-01-28 00:17:23 -0300

    Alteracao feita

--- src/Card.js
+++ src/Card.js
@@ -6,6 +6,7 @@ const Card = props => {
         <div className="card">
             <img src={props.avatar} alt="Avatar" style={{width: '100%'}}></img> 
             <div className="container">
+                <h4>Alteracao</h4>
                 <h4><b>{props.name}</b></h4> 
                 <input type='text' onChange={props.onChangeName} value={props.name} />
                 <p>{props.carrer}</p> 
